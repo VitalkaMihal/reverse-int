bbd32505869e22418a49defb5f18d5fa6e4ca45f
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2021-03-08 01:25:37 +0300

    feat: add reverse

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
 module.exports = function reverse (n) {
-    if (n < 0){
-        return Number((n * -1).toString().split('').reverse().join('')); 
-    } else {
-       return Number(n.toString().split('').reverse().join(''));  
-    }
- 
+    return Number(Math.abs(n).toString().split('').reverse().join(''));  
 }
